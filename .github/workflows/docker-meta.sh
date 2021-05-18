50520700ef806e16b638f4fd0fd0e681615ec90a
Andrew Hsu <xuzuan@gmail.com>
2021-05-18 09:15:36 -0500

    reverse order of docker image push

--- .github/workflows/docker-meta.sh
+++ .github/workflows/docker-meta.sh
@@ -6,16 +6,16 @@ IMG_TAGS=""
 if [[ $GITHUB_EVENT_NAME == 'push' ]]; then
 	IMG_VER=${GITHUB_SHA::7} # e.g. badbeef
 
-	# e.g. two tags: main, badbeef
-	IMG_TAGS="$IMG_NAME:main,$IMG_NAME:$IMG_VER"
+	# e.g. two tags: badbeef, main
+	IMG_TAGS="$IMG_NAME:$IMG_VER,$IMG_NAME:main"
 elif [[ $GITHUB_EVENT_NAME == 'release' ]]; then
 	if [[ "$GITHUB_REF" =~ ^refs/tags/v[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
 		IMG_VER=${GITHUB_REF#refs/tags/} # e.g. v1.2.3
 		IMG_MAJ_MIN=${IMG_VER%.*} # e.g. v1.2
 		IMG_MAJ=${IMG_MAJ_MIN%.*} # e.g. v1
 
-		# e.g. four tags: latest, v1, v1.2, v1.2.3
-		IMG_TAGS="$IMG_NAME:latest,$IMG_NAME:$IMG_MAJ,$IMG_NAME:$IMG_MAJ_MIN,$IMG_NAME:$IMG_VER"
+		# e.g. four tags: v1.2.3, v1.2, v1, latest
+		IMG_TAGS="$IMG_NAME:$IMG_VER,$IMG_NAME:$IMG_MAJ_MIN,$IMG_NAME:$IMG_MAJ,$IMG_NAME:latest"
 	else
 		echo 'Unknown GITHUB_REF format, cannot construct image tags.'
 	fi
